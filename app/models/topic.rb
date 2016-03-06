3f43b43aee4156709db8aa25cb0035112c260069
linaelsadek <linaelsadek@cmail.carleton.ca>
2016-03-06 00:46:09 +0000

    added dependency between topic and comment, so if topic is deleted, so does all the comments associated with it

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
   validates :author, presence: true, length: {minimum: 3}
 end
