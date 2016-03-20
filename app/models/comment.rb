c56e6352f61779494f65e9d57021fab3ca4f5c91
linaelsadek <linaelsadek@cmail.carleton.ca>
2016-03-20 20:02:39 +0000

    Modified the view of the Home page, added help, about, and contact pages

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :topic
   has_many :votes, dependent: :destroy
+  validates :author, presence: true, length: {minimum: 3}
+  validates :text, presence: true, length: {minimum: 10}
+
 end
