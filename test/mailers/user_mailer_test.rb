e0af27086fc473c993580024329faa791b7f6991
linaelsadek <linaelsadek@cmail.carleton.ca>
2016-03-31 21:42:06 +0000

    test run

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "account_activation" do
-    user = User.first
+    user = users(:lina)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
     assert_equal "Socialwiki Account Activation", mail.subject
@@ -12,7 +12,7 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "password_reset" do
-    user = User.first
+    user = users(:lina)
     user.activation_token = User.new_token
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
