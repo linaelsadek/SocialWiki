f8539bad02746ef2fe2c691271a75063d3320842
linaelsadek <linaelsadek@cmail.carleton.ca>
2016-03-31 21:29:49 +0000

    modified tests

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,18 +2,22 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "account_activation" do
-    mail = UserMailer.account_activation
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
     assert_equal "Socialwiki Account Activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@socialwiki.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
   test "password_reset" do
-    mail = UserMailer.password_reset
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@socialwiki.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
