4c0b47959efaa30300b1ae57b70ab4da6bdbb724
linaelsadek <linaelsadek@cmail.carleton.ca>
2016-03-31 22:11:58 +0000

    modified user_mailer_test.rb

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,7 +15,7 @@ class UserMailerTest < ActionMailer::TestCase
 
   test "password_reset" do
     user = users(:michael)
-    user.activation_token = User.new_token
+    user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
